1d07de7bea04f16beeee354cf3663049909b9a22
steven0608-code <orobiasteven3@gmail.com>
2025-03-05 00:29:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+//api key para makuha yung data sa openweather API
 const apiKey = "937f7a689a7f864acbb210086a24a975";
+
+
+//value ng city input para sa ididisplay na weather data
 
 function getWeather() {
     const city = document.getElementById("cityInput").value;
@@ -6,25 +10,32 @@ function getWeather() {
     const forecastContainer = document.getElementById("forecastContainer");
     const loading = document.getElementById("loading");
 
+
+    
     if (!city) {
         weatherResult.innerHTML = "<p style='color: red;'>Please enter a city name.</p>";
         return;
     }
 
+
+    //loading indicator
     loading.style.display = "block";
     weatherResult.innerHTML = "";
     forecastContainer.innerHTML = "";
 
 
+    //present weather
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
- 
+    
+    //5 day 
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
 
-    
+    //para makuha yung weather data para sa api
     fetch(weatherUrl)
         .then(response => response.json())
         .then(data => {
+            
+            //matatanggal pag nakuha na yung data
             loading.style.display = "none";
 
             if (data.cod === "404") {
@@ -40,7 +51,7 @@ function getWeather() {
             const sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
             const sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString();
 
-         
+         //update para makita ang present weather
             weatherResult.innerHTML = `
                 <h2>${data.name}, ${data.sys.country}</h2>
                 <img class="weather-icon" src="${icon}" alt="Weather Icon">
@@ -62,6 +73,8 @@ function getWeather() {
         .then(data => {
             if (data.cod !== "200") return;
 
+
+            //Kinukuha ang forecast data at tinitignan kung valid ito.
             const dailyForecasts = {};
             data.list.forEach(entry => {
                 const date = new Date(entry.dt * 1000).toDateString();
@@ -73,6 +86,7 @@ function getWeather() {
           
             const forecastEntries = Object.values(dailyForecasts).slice(0, 5);
 
+            
             forecastEntries.forEach(entry => {
                 const temp = entry.main.temp;
                 const icon = `https://openweathermap.org/img/wn/${entry.weather[0].icon}.png`;
